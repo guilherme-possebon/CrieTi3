11317a714ef64e3160e71b9803ffc40a66314990
Guilherme Possebon <gpossebon67@gmail.com>
2024-09-14 14:05:39 -0300

    Adicionando a unidade de medida no front

--- Aulas do Fabricio/ex1/client/script.js
+++ Aulas do Fabricio/ex1/client/script.js
@@ -72,7 +72,7 @@ async function showPaymentOptions() {
     let paymentOption = paymentsOptions[index];
     if (paymentOption !== null) {
       console.log(paymentOption, 123123123);
-      let editar = `<button onclick="goToEditPage(${paymentOption.id})" class="btn btn-success">Editar</button>`;
+      let editar = `<button onclick="goToPaymentEditPage(${paymentOption.id})" class="btn btn-success">Editar</button>`;
       let excluir = `<button onclick="deletePaymentOption(${paymentOption.id})" class="btn btn-danger">Excluir</button>`;
       html += `
         <tr class="table-row-hover">
@@ -88,7 +88,7 @@ async function showPaymentOptions() {
   document.getElementById("tbodyFormasPagamentos").innerHTML = html;
 }
 
-// NOTE save paymento option
+// NOTE Save paymento option
 async function savePaymentOption() {
   let id = getParam("id");
   let method = id == null ? "POST" : "PUT";
@@ -109,13 +109,13 @@ async function savePaymentOption() {
 
   try {
     let result = await fetch(apiUrl + url, requestOptions);
-    let saveResult = await result.json();
+    let json = await result.json();
 
-    if (saveResult) {
+    if (result.ok) {
       alert("Salvo com sucesso!");
       window.location = "/client/formapagamento.html";
     } else {
-      alert("Erro ao salvar a forma de pagamento");
+      alert("Erro ao salvar a forma de pagamento, erro: " + json.erro);
     }
   } catch (error) {
     console.error("Error:", error);
@@ -123,9 +123,13 @@ async function savePaymentOption() {
   }
 }
 
-// NOTE Go to edit page
-function goToEditPage(id) {
+// NOTE Go to payment edit page
+function goToPaymentEditPage(id) {
   window.location = "/client/adicionarformapagameto.html?id=" + id;
+}
+// NOTE Go to unit edit page
+function goToUnitEditPage(id) {
+  window.location = "/client/adicionarunidademedida.html?id=" + id;
 }
 
 // NOTE Get param
@@ -175,6 +179,108 @@ async function deletePaymentOption(id) {
     console.log(deleteResult);
 
     showPaymentOptions();
+  }
+}
+
+// NOTE Show unit of measurement
+async function showUnitOfMeasurement() {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  let result = await fetch(apiUrl + "/unidadeMedida", requestOptions);
+  let unitOptions = await result.json();
+  let html = "";
+
+  for (let index = 0; index < unitOptions.length; index++) {
+    let unitOption = unitOptions[index];
+    if (unitOption !== null) {
+      let editar = `<button onclick="goToUnitEditPage(${unitOption.id})" class="btn btn-success">Editar</button>`;
+      let excluir = `<button onclick="deleteUnit(${unitOption.id})" class="btn btn-danger">Excluir</button>`;
+      html += `
+        <tr class="table-row-hover">
+          <td class="table-body-cell">${unitOption.id}</td>
+          <td class="table-body-cell">${unitOption.name}</td>
+          <td class="table-body-cell">${editar}</td>
+          <td class="table-body-cell">${excluir}</td>
+        </tr>
+      `;
+      console.log(unitOption);
+    }
+  }
+  document.getElementById("tbodyUnidadeMedida").innerHTML = html;
+}
+
+// NOTE Save unit of measurement
+async function saveUnitOfMeasurement() {
+  let id = getParam("id");
+  let method = id == null ? "POST" : "PUT";
+  let url = id == null ? "/unidademedida" : "/unidademedida/" + id;
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let unitOption = { name: document.getElementById("name").value };
+  const raw = JSON.stringify(unitOption);
+
+  const requestOptions = {
+    method: method,
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  try {
+    let result = await fetch(apiUrl + url, requestOptions);
+    let json = await result.json();
+
+    if (result.ok) {
+      alert("Salvo com sucesso!");
+      window.location = "/client/unidademedida.html";
+    } else {
+      alert("Erro ao salvar a unidade de medida, erro: " + json.erro);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Ocorreu um erro ao tentar salvar. Tente novamente mais tarde.");
+  }
+}
+
+// NOTE Load unit of measurement
+async function loadUnitOfMeasurement() {
+  let id = getParam("id");
+
+  if (id !== null) {
+    document.getElementById("h1").innerHTML = "Editar forma de pagamento";
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    let result = await fetch(apiUrl + "/unidademedida/" + id, requestOptions);
+    let paymentoResult = await result.json();
+
+    document.getElementById("name").value = paymentoResult.name;
+
+    console.log(paymentoResult);
+  }
+}
+
+// NOTE Delete unit of measurement
+async function deleteUnit(id) {
+  if (confirm("Deseja realmente deletar a unidade de medida do id: " + id)) {
+    const requestOptions = {
+      method: "DELETE",
+      redirect: "follow",
+    };
+
+    let result = await fetch(apiUrl + "/unidademedida/" + id, requestOptions);
+    let deleteResult = await result.json();
+
+    console.log(deleteResult);
+
+    showUnitOfMeasurement();
   }
 }
 
